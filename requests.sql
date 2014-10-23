5d8922a7f831b441c5579e6ab9cdcc8f0530b5ed
Florent Benoit <Florent.Benoit@serli.com>
2014-10-23 11:51:58 +0200

    Configure to use MySQL by default

---
+++ requests.sql
@@ -0,0 +1,6 @@
+select * from petclinic.owners order by id desc;
+
+INSERT INTO petclinic.owners (first_name, last_name, address, city, telephone) VALUES ('Florent', 'Benoit', 'Main Street', 'Grenoble', '0102030405');
+
+select * from petclinic.owners order by id desc;
+
